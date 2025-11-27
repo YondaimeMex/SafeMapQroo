7595cf2a1d95cb2f71422910cf4733104219abce
GabrielMay <44277883+gabrielmayek@users.noreply.github.com>
2025-11-27 15:15:39 -0500

    cambios nuevos

--- src/api/Requests/shelter/DeleteShelterHook.jsx
+++ src/api/Requests/shelter/DeleteShelterHook.jsx
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:src/Api/Requests/shelter/DeleteShelter.jsx
 import { useState } from "react";
 import { apiClient } from "../generateapi";
 
@@ -23,29 +22,3 @@ export const useDeleteShelter = () => {
 
     return { loading, error, deleteShelter };
 };
-========
-import { useState } from "react";
-import { apiClient } from "../../generateapi";
-
-export const useDeleteShelter = () => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    const deleteShelter = async (id) => {
-        setLoading(true);
-        setError(null);
-
-        try {
-            await apiClient.delete(`/shelters/${id}`);
-            return true;
-        } catch (err) {
-            setError(err);
-            throw err;
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    return { loading, error, deleteShelter };
-};
->>>>>>>> d42c324599e50cebc445eb07eb805799307673b8:src/Api/Requests/shelter/DeleteShelterHook.jsx
